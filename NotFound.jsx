e92ef1a3426ba64faadb62cff54cb4bddec9d655
zo5NuNTr <sac2427bca2892@ssacollegechennai.com>
2025-09-17 12:01:32 +0100

    Add files via upload

---
+++ NotFound.jsx
@@ -0,0 +1,9 @@
+export default function NotFound(){
+    return(
+        <div className="m-2 text-center">
+            <h1>Page Not Found!</h1>
+            <p className="text-8xl">404</p>
+            <p>You may have typed a wrong URL...</p>
+        </div>
+    )
+}
